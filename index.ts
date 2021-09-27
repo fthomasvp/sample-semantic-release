4ce417e0ec5f229c6033ac5618cfa9ce29d3e134
fthomasvp <fellipecab@gmail.com>
2021-09-27 04:00:30 -0300

    feat: add hi endpoint

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ app.get("/", (req, res) => res.send("Express + TypeScript Server !"));
 
 app.get("/hello", (req, res) => res.send("Um novo endpoint foi adicionado :)"));
 
+app.get("/hi", (req, res) => res.send("Hi You!"));
+
 app.listen(PORT, () => {
   console.log(`Server is running at https://localhost:${PORT}`);
 });
